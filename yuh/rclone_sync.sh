50a429f37b9795bac8a9f63991debcff707ab4bc
carl125 <huyhn1321@gmail.com>
2024-01-23 21:51:00 +0700

    - prepare 3 cloud coresponding 3 camera - implement retries call when work with ffmpeg

--- yuh/rclone_sync.sh
+++ yuh/rclone_sync.sh
@@ -3,21 +3,25 @@
 # Base directory for camera recordings
 base_directory="/etc/record/camera"
 
-# Camera directories
-declare -a camera_dirs=("kitchen" "living_room" "outdoor")
+# Camera directories and their respective cloud destinations
+declare -A camera_cloud_map
+camera_cloud_map["kitchen"]="one-drive:camera/kitchen"
+camera_cloud_map["living_room"]="one-drive2:camera/living_room"
+camera_cloud_map["outdoor"]="one-drive3:camera/outdoor"
+
 declare -a pids
 declare -A pid_file_map
 
 # Function to handle the upload process for each camera directory
 upload_camera_dir() {
     local camera_dir=$1
-    local cloud_dir="one-drive:camera/$camera_dir"
+    local cloud_dir=${camera_cloud_map[$camera_dir]}
 
     for file in "$base_directory/$camera_dir"/*; do
         if [ -f "$file" ]; then
-             # Extract the date from the file name
+            # Extract the date from the file name
             file_date=$(basename "$file" | awk -F '--' '{print $1}')
-            
+
             # Start rclone copy in the background and save its PID
             rclone copy "$file" "$cloud_dir"/"$file_date" &
             pid=$!
@@ -28,7 +32,7 @@ upload_camera_dir() {
 }
 
 # Iterate over each camera directory and upload files
-for dir in "${camera_dirs[@]}"; do
+for dir in "${!camera_cloud_map[@]}"; do
     upload_camera_dir "$dir"
 done
 
--- yuh/record_outdoor.sh
+++ yuh/record_outdoor.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 temp_camera="/etc/record/temp/outdoor"
-date_now=$(date +\%d-\%m-\%Y--\%H-\%M)
-cam_stream_url=http://77.222.181.11:8080/mjpg/video.mjpg
+cam_stream_url="http://77.222.181.11:8080/mjpg/video.mjpg"
+max_retries=3
+retry_interval=10  # seconds
 
-# Record for 55 second to the temporary directory
-sudo ffmpeg -i "$cam_stream_url" -vcodec copy -t 55 -y "$temp_camera"/$date_now.mp4
+# Record for 95 seconds to the temporary directory with retry logic
+for ((i=1; i<=max_retries; i++)); do
+    # Update the timestamp for each attempt
+    date_now=$(date +\%d-\%m-\%Y--\%H-\%M)
 
+    echo "Attempt $i of $max_retries to record the stream..."
+    sudo ffmpeg -i "$cam_stream_url" -vcodec copy -t 95 -y "$temp_camera"/$date_now.mp4
+
+    # Check if ffmpeg was successful
+    if [ $? -eq 0 ]; then
+        echo "yuh-s: Recording successful."
+        break  # Exit the loop if recording was successful
+    else
+        echo "yuh-e: Recording failed. Server returned error. Retrying in $retry_interval seconds..."
+        sleep $retry_interval
+    fi
+done
+
+# If recording was not successful after retries, log an error
+if [ $i -gt $max_retries ]; then
+    echo "yuh-e: Recording failed after $max_retries attempts."
+fi
